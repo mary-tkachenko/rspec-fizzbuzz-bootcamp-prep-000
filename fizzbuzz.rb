b2d9fb6c4e1e30e5720ba9b501c91f56f82c087e
Mary <l.skyling@gmail.com>
2018-03-29 02:27:13 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,11 @@
 def fizzbuzz(number)
   if number % 3 == 0 && number % 5 == 0
     return "FizzBuzz"
-
+  end
+  
   if number % 5 == 0
     return "Buzz"
+  end
 
   if number % 3 == 0
     return "Fizz"
